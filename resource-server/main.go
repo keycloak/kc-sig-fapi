30502323ec018e41bf865a1128160f1acc5536b1
Hiroyuki Wada <h2-wada@nri.co.jp>
2019-06-28 16:36:53 +0900

    Fix #8 Resource Server related FAILURE : Content-type HTTP header

--- resource-server/main.go
+++ resource-server/main.go
@@ -22,7 +22,7 @@ func accountsHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.Write(j)
 }
 
