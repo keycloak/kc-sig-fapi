a1d6175c85a19b414fb429ca1c29e074046f90ee
Takashi Norimatsu <takashi.norimatsu.ws@hitachi.com>
2024-11-08 18:09:18 +0900

    Support automatic FAPI2 SP ID2 DPoP-MTLS-OAuth conformance test
    closes #660
    
    Signed-off-by: Takashi Norimatsu <takashi.norimatsu.ws@hitachi.com>

--- conformance-tests-env/conformance-suite/conformance-suite/run-tests.sh
+++ conformance-tests-env/conformance-suite/conformance-suite/run-tests.sh
@@ -493,7 +493,7 @@ makeManualOIDCRpInitiatedLogoutTest() {
 }
 
 makeFAPI2SPTest() {
-    #TESTS="${TESTS} fapi2-security-profile-id2-test-plan[sender_constrain=dpop][client_auth_type=mtls][openid=plain_oauth][fapi_profile=plain_fapi] ../conformance-suite/.gitlab-ci/fapi-conformance-suite-configs/fapi2-sp-id2/fapi2-ID2-FAPI2SP-DPOP-MTLS-MTLS-ES256-ES256-automated.json"
+    TESTS="${TESTS} fapi2-security-profile-id2-test-plan[sender_constrain=dpop][client_auth_type=mtls][openid=plain_oauth][fapi_profile=plain_fapi] ../conformance-suite/.gitlab-ci/fapi-conformance-suite-configs/fapi2-sp-id2/fapi2-ID2-FAPI2SP-DPOP-MTLS-MTLS-ES256-ES256-automated.json"
     TESTS="${TESTS} fapi2-security-profile-id2-test-plan[sender_constrain=mtls][client_auth_type=mtls][openid=plain_oauth][fapi_profile=plain_fapi] ../conformance-suite/.gitlab-ci/fapi-conformance-suite-configs/fapi2-sp-id2/fapi2-ID2-FAPI2SP-MTLS-MTLS-ES256-ES256-automated.json"
     TESTS="${TESTS} fapi2-security-profile-id2-test-plan[sender_constrain=mtls][client_auth_type=private_key_jwt][openid=plain_oauth][fapi_profile=plain_fapi] ../conformance-suite/.gitlab-ci/fapi-conformance-suite-configs/fapi2-sp-id2/fapi2-ID2-FAPI2SP-private-key-MTLS-PS256-PS256-automated.json"
     TESTS="${TESTS} fapi2-security-profile-id2-test-plan[sender_constrain=mtls][client_auth_type=mtls][openid=openid_connect][fapi_profile=plain_fapi] ../conformance-suite/.gitlab-ci/fapi-conformance-suite-configs/fapi2-sp-id2/fapi2-ID2-FAPI2SP-OpenID-Connect-ES256-ES256-automated.json"
